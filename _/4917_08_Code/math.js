2fa537732907662b421375df4e765b22afbd16f6
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-01-02 18:46:50 -0800

    I

---
+++ _/4917_08_Code/math.js
@@ -0,0 +1,7 @@
+import * as logarithm from "math_modules/logarithm.js";
+import * as trigonometry from "math_modules/trigonometry.js";
+
+export default {
+	logarithm: logarithm,
+	trigonometry: trigonometry
+};
